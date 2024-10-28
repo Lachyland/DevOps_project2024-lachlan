72c439f4eef3b2c4c192ecc2be6310a774ddac77
Lachlan <locklanloh@gmail.com>
2024-10-28 17:12:22 +0800

    Added connection to MongoDb

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var express = require('express')
 var bodyParser = require("body-parser");
 var app = express();
 
+const mongoose = require('mongoose');
+require('dotenv').config();
 const PORT = process.env.PORT || 5050
 var startPage = "index.html";
-
+mongoose.set('strictQuery', true);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
@@ -13,11 +15,20 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/" + startPage);
 })
 
+mongoose.connect(process.env.DB_CONNECT)
+    .then(() => {
+        console.log("MongoDB connected");
+    })
+    .catch(err => {
+        console.error("MongoDB connection error:", err);
+    });
+
 server = app.listen(PORT, function () {
     const address = server.address();
     const baseUrl = `http://${address.address == "::" ? 'localhost' :
         address.address}:${address.port}`;
     console.log(`Demo project at: ${baseUrl}`);
 });
+
 
 module.exports = { app, server }
